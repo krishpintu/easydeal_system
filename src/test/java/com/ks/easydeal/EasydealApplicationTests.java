4eea8edad4e11ad077d629e40240d6567d4c0768
krishna <krishpintu@gmail.com>
2022-06-21 23:26:38 +0530

    Initial commit

---
+++ src/test/java/com/ks/easydeal/EasydealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.easydeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasydealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
